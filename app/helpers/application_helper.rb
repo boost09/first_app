e4aeacd786b54f8967b82ca1da19a3bdd43fd36a
Jay Compton <boost09@gmail.com>
2013-09-26 19:16:38 -0400

    added blueprint css framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a title for each page
+  def title
+    base_title = "jay compton"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
