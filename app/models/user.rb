224d70f593ca08dc3537152e6543f0c22aceef3f
Jay Compton <boost09@gmail.com>
2014-04-10 10:46:08 -0400

    corrected test for nonexistent user

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
 
   # Return true if the user's password matches the submitted password
   def has_password?(submitted_password)
-	encrypted_password == encrypt(submitted_password)	
+	self.encrypted_password == encrypt(submitted_password)	
   end
 
   def self.authenticate(email, submitted_password)
