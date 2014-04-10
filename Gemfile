479615138b63cef5ed3f5c520814931425a625bd
Jay Compton <boost09@gmail.com>
2014-04-10 12:46:52 -0400

    set factory_girl require to false and added spec_helper to correct rake abort

--- Gemfile
+++ Gemfile
@@ -46,4 +46,4 @@ group :development, :test do
   gem 'rspec-rails',      ">= 2.0.0.beta"
 end
 
-gem 'factory_girl_rails'
+gem 'factory_girl_rails', :require => false
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,10 @@
 # Using the symbol ':user' allows factory_girl to simulate the User model
 
-#FactoryGirl.define do
-#	factory :user do
-#  		user.name			"Jay Compton"
-#  		user.email			"boost09@gmail.com"
-#  		user.password			"foobar"
-#  		user.password_confirmation	"foobar"
-#	end
-#end
+FactoryGirl.define do
+	factory :user do
+		user.name			"Jay Compton"
+		user.email			"boost09@gmail.com"
+		user.password			"foobar"
+		user.password_confirmation	"foobar"
+	end
+end
